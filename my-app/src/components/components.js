195a075d5ab4e3884612a0a96a4a6cb9437a00f7
Steve <steve@simpson.com>
2022-11-01 14:15:21 +0000

    App setup complete

---
+++ my-app/src/components/components.js
@@ -0,0 +1,24 @@
+// src/components/contacts.js
+
+import React from 'react'
+import Card from 'react-bootstrap/Card';
+
+const Contacts = ({ contacts }) => {
+    return (
+        <div>
+            <center><h1>Contact List</h1></center>
+            {contacts.map((contact) => (
+                <Card>
+                    <Card.Body>
+                        <Card.Title>{contact.name}</Card.Title>
+                        <Card.Subtitle>{contact.email}</Card.Subtitle>
+                        <Card.Text>{contact.company.catchPhrase}</Card.Text>
+                    </Card.Body>
+                </Card>
+
+            ))}
+        </div>
+    )
+};
+
+export default Contacts
